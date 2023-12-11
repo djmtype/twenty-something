36794fc02ec901fe2b6499ad16059af74b4ecb09
Scott Rod <scott@spinlinestudio.com>
2023-12-11 17:42:59 -0500

    Added alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,26 @@ import { site } from './src/data/config.json';
 import remarkUnwrapImages from "remark-unwrap-images";
 import sitemap from '@astrojs/sitemap';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
-	site: site.url,
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		// Applied to .md and .mdx files
-		remarkPlugins: [remarkUnwrapImages],
-	  },
-	vite: {
-		server: {
-			watch: {
-				ignored: ['_backup/**']
-			  },
-			  
-		},
-		plugins: [],
-		ssr: {},
-		css: {
-		  devSourcemap: true
-		}
-	  },
+  site: site.url,
+  integrations: [mdx(), sitemap(), alpinejs()],
+  markdown: {
+    // Applied to .md and .mdx files
+    remarkPlugins: [remarkUnwrapImages]
+  },
+  vite: {
+    server: {
+      watch: {
+        ignored: ['_backup/**']
+      }
+    },
+    plugins: [],
+    ssr: {},
+    css: {
+      devSourcemap: true
+    }
+  }
 });
