595145dd09f3ddcf5b8c5e5bd5bf872386745494
Scott Rod <scott@spinlinestudio.com>
2024-03-01 12:23:58 -0500

    Update color

--- astro.config.ts
+++ astro.config.ts
@@ -7,8 +7,11 @@ import mdx from "@astrojs/mdx";
 import config from "./src/data/config.json";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import pageInsight from "astro-page-insight";
+const {
+  url: siteUrl
+}: ConfigObj["site"] = config.site;
 
-const { url: siteUrl }: ConfigObj["site"] = config.site;
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,57 +20,55 @@ export default defineConfig({
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "css-variables",
-      wrap: true,
+      wrap: true
     },
-    remarkPlugins: [remarkCallout, [remarkToc, { maxDepth: 2 }]],
+    remarkPlugins: [remarkCallout, [remarkToc, {
+      maxDepth: 2
+    }]]
   },
   integrations: [
-    // AutoImport({
-    //   imports: [
-    //     { "astro-icon/components": ["Icon"] },
-    //     "./src/components/ButtonLink.astro",
-    //     "./src/components/Card.astro",
-    //     "./src/components/Grid.astro",
-    //     "./src/components/Group.astro",
-    //     "./src/components/Container.astro",
+  // AutoImport({
+  //   imports: [
+  //     { "astro-icon/components": ["Icon"] },
+  //     "./src/components/ButtonLink.astro",
+  //     "./src/components/Card.astro",
+  //     "./src/components/Grid.astro",
+  //     "./src/components/Group.astro",
+  //     "./src/components/Container.astro",
 
-    //     // "./src/components/Title.astro",
+  //     // "./src/components/Title.astro",
 
-    //     // "./src/components/Figure.astro",
+  //     // "./src/components/Figure.astro",
 
-    //     // "./src/components/Column.astro",
-    //     // "./src/components/Heading.astro",
+  //     // "./src/components/Column.astro",
+  //     // "./src/components/Heading.astro",
 
-    //     // "./src/components/List.astro",
-    //     // "./src/components/Testimonial.astro",
-    //     // "./src/components/PostList.astro",
-    //     // "./src/components/NewsletterSignup.astro",
-    //     // "./src/components/Hero.astro",
-    //     // "./src/components/Callout.astro",
-    //   ],
-    // }),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: "css-variables",
-        // wrap: true,
-      },
-    }),
-    sitemap(),
-    icon({
-      include: {
-        lucide: ["*"],
-      },
-    }),
-  ],
-
+  //     // "./src/components/List.astro",
+  //     // "./src/components/Testimonial.astro",
+  //     // "./src/components/PostList.astro",
+  //     // "./src/components/NewsletterSignup.astro",
+  //     // "./src/components/Hero.astro",
+  //     // "./src/components/Callout.astro",
+  //   ],
+  // }),
+  mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "css-variables"
+      // wrap: true,
+    }
+  }), sitemap(), icon({
+    include: {
+      lucide: ["*"]
+    }
+  }), pageInsight()],
   output: "static",
   vite: {
     server: {},
     plugins: [],
     ssr: {},
     css: {
-      devSourcemap: true,
-    },
-  },
+      devSourcemap: true
+    }
+  }
 });
